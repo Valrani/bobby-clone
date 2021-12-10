b2787db593e11d8919bd4e799b317177005c99b6
antoinederoose <antoinederoose@yahoo.fr>
2021-12-10 11:55:06 +0100

    Initial Commit

---
+++ BobbyClone/BobbyCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  BobbyCloneApp.swift
+//  BobbyClone
+//
+//  Created by Antoine De Roose on 10/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct BobbyCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
