827027c42b0c6b2963fdc7f23ae063c0a38df26e
antoinederoose <antoinederoose@yahoo.fr>
2021-12-13 22:48:42 +0100

    refactoring

---
+++ BobbyClone/Views/SubscriptionCreation/SubscriptionCreationView.swift
@@ -0,0 +1,20 @@
+//
+//  SubscriptionCreationView.swift
+//  BobbyClone
+//
+//  Created by Antoine De Roose on 13/12/2021.
+//
+
+import SwiftUI
+
+struct SubscriptionCreationView: View {
+  var body: some View {
+    Text("Subscription Creation")
+  }
+}
+
+struct SubscriptionCreationView_Previews: PreviewProvider {
+  static var previews: some View {
+    SubscriptionCreationView()
+  }
+}
