f265f72fe65ea51c6fe6f566ba99cb9b6d9616e6
antoinederoose <antoinederoose@yahoo.fr>
2021-12-18 18:46:55 +0100

    template view with 3 tabs

--- BobbyClone/Views/Templates/TemplatesTopView.swift
+++ BobbyClone/Views/Templates/TemplatesTopView.swift
@@ -8,13 +8,48 @@
 import SwiftUI
 
 struct TemplatesTopView: View {
+  
+  let onChangeViewDisplayed: (_ newValue: Int) -> Void
+  
+  @Environment(\.dismiss) private var dismissTemplatesView
+  
   var body: some View {
-    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    VStack(spacing: 0) {
+      HStack {
+        Button(action: { dismissTemplatesView() }) {
+          Image(systemName: "chevron.down")
+            .font(Font.body.weight(.semibold))
+        }
+        Spacer()
+        Text("Ajouter un abonnement")
+          .fontWeight(.semibold)
+          .lineLimit(1)
+        Spacer()
+        Button(action: { dismissTemplatesView() }) {
+          Image(systemName: "magnifyingglass")
+            .font(Font.body.weight(.semibold))
+        }
+      }
+      .padding()
+      HStack {
+        Text("Go All")
+          .onTapGesture { onChangeViewDisplayed(0) }
+        Spacer()
+        Text("Go Popular")
+          .onTapGesture { onChangeViewDisplayed(1) }
+        Spacer()
+        Text("Go Nibba")
+          .onTapGesture { onChangeViewDisplayed(2) }
+      }
+      .padding()
+      LineSeparator()
+    }
+    .ignoresSafeArea()
   }
 }
 
-struct TemplatesTopView_Previews: PreviewProvider {
-  static var previews: some View {
-    TemplatesTopView()
-  }
-}
+//struct TemplatesTopView_Previews: PreviewProvider {
+//  static var previews: some View {
+//    TemplatesTopView()
+//  }
+//}
--- BobbyClone/Views/Templates/TemplatesView.swift
+++ BobbyClone/Views/Templates/TemplatesView.swift
@@ -17,37 +17,45 @@ import SwiftUI
 /// Diminuer l'offset du HStack le fait se décaler vers la droite, ce qui permet au final d'aller faire glisser la 2eme vue etc.
 struct TemplatesView: View {
   
-  /// Numbers of view, where the first is 0, the second is 1, the third is 2...
-  @State var viewDisplayed = 0
-  /// The offset applied to the HStack to display the correct view
-  @State private var offset: CGFloat = 0
+  /// View displayed when the view appear, where the first is 0, the second is 1 etc.
+  @State private var viewDisplayed = 0
+  /// Number of tabs
+  private let numberOfTabs = 3
+  /// The offset applied to the HStack to display the correct view (computed in onAppear)
+  @State private var offset: CGFloat = .nan
   
   var body: some View {
     ScrollView {
       // Top View
-      HStack {
-        Text("Go All")
-          .onTapGesture { viewDisplayed = 0 }
-        Spacer()
-        Text("Go Popular")
-          .onTapGesture { viewDisplayed = 1 }
-        Spacer()
-        Text("Go Nibba")
-          .onTapGesture { viewDisplayed = 2 }
-      }
-      .padding()
-      .background(.cyan)
+//      HStack {
+//        Text("Go All")
+//          .onTapGesture { viewDisplayed = 0 }
+//        Spacer()
+//        Text("Go Popular")
+//          .onTapGesture { viewDisplayed = 1 }
+//        Spacer()
+//        Text("Go Nibba")
+//          .onTapGesture { viewDisplayed = 2 }
+//      }
+//      .padding()
+//      .background(.cyan)
+      TemplatesTopView(onChangeViewDisplayed: { newValue in
+        viewDisplayed = newValue
+      })
       
       // Main View
       GeometryReader { gr in
         Group {
-          HStack {
+          HStack(spacing: 0) {
             All()
               .frame(width: gr.size.width)
+              .background(.red)
             Popular()
               .frame(width: gr.size.width)
+              .background(.green)
             Nibba()
               .frame(width: gr.size.width)
+              .background(.gray)
           }
           .offset(x: offset)
         }
@@ -62,99 +70,14 @@ struct TemplatesView: View {
           offset = -CGFloat(viewDisplayed) * gr.size.width
         }
         // Recompute the offset if the device orientation change.
-        .onRotate { _ in
-          // TODO: bug
-          print("uiscreen bounds with : \(UIScreen.main.bounds.width)")
-          print("old offet -> \(offset)")
+        .onRotate { newOrientation in
           offset = -CGFloat(viewDisplayed) * UIScreen.main.bounds.width
-          print("new offset -> \(offset)")
         }
       }
     }
-    
+    .edgesIgnoringSafeArea(.horizontal)
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-//struct TemplatesView: View {
-//
-//  /// Numbers of view, where the first is 0, the second is 1, the third is 2...
-//  @State private var viewDisplayed = 1
-//  /// The offset applied to the HStack to display the correct view
-//  @State private var offset: CGFloat = 0
-//
-//  var body: some View {
-//    VStack {
-//      GeometryReader { gr in
-//        Group {
-//          HStack {
-//            All() {
-//              withAnimation {
-//                viewDisplayed = 1
-//                offset -= gr.size.width
-//              }
-//            }
-//            .frame(width: gr.size.width)
-//            Popular() {
-//              withAnimation {
-//                viewDisplayed = 0
-//                offset += gr.size.width
-//              }
-//            }
-//            .frame(width: gr.size.width)
-//          }
-//          .offset(x: offset)
-//        }
-//        .onAppear {
-//          offset = -CGFloat(viewDisplayed) * gr.size.width
-//        }
-//        .onRotate { _ in
-//          offset = -CGFloat(viewDisplayed) * gr.size.width
-//        }
-//      }
-//    }
-//
-//    private func moveLeft(grWidth: CGFloat) {
-//      // TODO: go fish
-//    }
-//
-//    private func moveRight(grWidth: CGFloat) {
-//      // TODO: go fish
-//    }
-//
-//  }
-//}
-
-
-
-
-
-
-
-
-
-
-
-
-
-//        if (viewDisplayed == 0) {
-//          print("nothing to do bcs offset == \(offset)")
-//        }
-//        if (viewDisplayed == 1) {
-//          print("current offset -> \(offset)")
-//          print("gr.size.width -> \(gr.size.width)")
-//          offset = -gr.size.width
-//        }
 
 struct All: View {
   var body: some View {
